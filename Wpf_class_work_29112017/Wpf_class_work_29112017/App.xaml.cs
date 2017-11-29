606db1fd4fd1f0e9181e729ad0ec08345f9b8dca
Alexandr Momotuik <sasha.momotuik@gmail.com>
2017-11-29 18:23:30 +0200

    start

---
+++ Wpf_class_work_29112017/Wpf_class_work_29112017/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_class_work_29112017
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
